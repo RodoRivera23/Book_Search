79d59687162aec67ca24256526ee40c8b1c55da3
Rodo Rivera <rudy_roddick@msn.com>
2023-11-14 20:00:56 -0600

    first commit for server side

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,45 @@
+const { User, bookSchema } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id });
+            }
+            throw AuthenticationError;
+        },
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveBook: async (
+            parent,
+            { authors, description, title, bookId, image, link },
+            context
+        ) => { },
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        removeBook: async (parent, { bookId }) => { },
+    },
+};
+
+module.exports = resolvers;
