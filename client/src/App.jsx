6eec032eb4f4f7687f200fef4c4ec5f05953050f
Rodo Rivera <rudy_roddick@msn.com>
2023-11-14 19:38:34 -0600

    First commit to the client side

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import { } from '@apollo/client'
+
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
