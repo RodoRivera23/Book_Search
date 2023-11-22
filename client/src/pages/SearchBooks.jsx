5d1bedd14cf6b89f3274a5615c10a247c41b88a4
Rodo Rivera <rudy_roddick@msn.com>
2023-11-22 16:20:12 -0600

    Update dependencies in package.json; fix typo in import in components

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { Container, Col, Form, Button, Card, Row } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { saveBook, searchGoogleBooks } from '../utils/Api';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
